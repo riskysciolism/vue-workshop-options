af3c0acb31a5555c9e4262c32b7eac1a15696750
riskysciolism <richard.musebrink@gmail.com>
2020-12-15 11:00:42 +0100

    INITIAL

---
+++ src/components/item.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="view">
+    <input class="toggle" type="checkbox" 
+    v-on:click="complete" v-bind:checked="todo.complete">
+    <label>{{todo.title}}</label>
+    <button class="destroy" v-on:click="remove"></button>
+</div>
+</template>
+
+<script>
+export default {
+    name: "item",
+    props: {
+        todo: Object
+    },
+    methods: {
+        remove() {
+            this.$emit('remove', this.todo);
+        },
+        complete() {
+            this.$emit('complete', this.todo);
+        }
+    }
+}
+</script>
